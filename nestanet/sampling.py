0b7bd9cacde74d592e0ac38d04fb92a7b29d78a2
Maksym Neyra-Nesterenko <mneyrnest@proton.me>
2022-12-12 17:18:21 -0800

    add stacking scheme
    Adds the stacking sampling scheme for the Bernoulli model. To be used in
    relation to master's thesis.

--- nestanet/sampling.py
+++ nestanet/sampling.py
@@ -62,9 +62,42 @@ def optimal_hist_2d(N):
     hist = (bar_vals.astype(float))**(-2)
 
     return hist
+
+def stacked_scheme_2d(mask1, mask2, N1, N2):
+    """
+    Compute the stacked scheme mask from two sampling masks of size N1xN2.
+
+    Returns the stacked mask, the submasks used to correctly define the
+    measurements y1 and y2 (referred to as u1_mask, u2_mask, respectively)
+    and the correct permutations of the measurements (perm1 and perm2).
+    """
+    idxs_1 = np.where(mask1)
+    idxs_2 = np.where(mask2)
+
+    idxs_1_flat = np.ravel_multi_index(idxs_1, (N1,N2))
+    idxs_2_flat = np.ravel_multi_index(idxs_1, (N1,N2))
+
+    omega_2_diff_1 = np.setdiff1d(idxs_2_flat, idxs_1_flat, assume_unique=True)
+    omega_1 = np.concatenate((idxs_1_flat, omega_2_diff_1))
+    u2_mask = np.isin(idxs_2_flat, omega_2_diff_1)
+
+    omega_1_diff_2 = np.setdiff1d(idxs_1_flat, idxs_2_flat, assume_unique=True)
+    omega_2 = np.concatenate((idxs_2_flat, omega_1_diff_2))
+    u1_mask = np.isin(idxs_1_flat, omega_1_diff_2)
+
+    o1, perm1 = np.sort(omega_1), np.argsort(omega_1)
+    o2, perm2 = np.sort(omega_2), np.argsort(omega_2)
+
+    assert np.all(o1 == o2) == True
+
+    mask = np.zeros(N1*N2, dtype=bool)
+    mask[o1] = True
+    mask = mask.reshape((N1,N2))
+
+    return mask, u1_mask, u2_mask, perm1, perm2
     
-def gaussian_multilevel_sampling(N):
-    raise NotImplementedError('Gaussian multilevel sampling not available')
+#def gaussian_multilevel_sampling(N):
+#    raise NotImplementedError('Gaussian multilevel sampling not available')
 
 def _constraint(t, m, hist):
     terms = t*m*hist
